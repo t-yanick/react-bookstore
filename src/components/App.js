ab64b0be243473d946f369b35d9316a778ffffbd
Yanick <tyankee15@gmail.com>
2021-05-31 14:15:31 -0300

    Implement App component

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import BookList from '../containers/BookList';
+import BookForm from '../containers/BookForm';
+
+const App = () => (
+  <div>
+    <BookList />
+    <BookForm />
+  </div>
+)
