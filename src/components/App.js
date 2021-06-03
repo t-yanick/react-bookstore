efb7e3ad5f67b4206d7cbb522316732e208c621d
Yanick <tyankee15@gmail.com>
2021-06-03 12:55:41 -0300

    Add CSS styles to Book.js component

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@
 import React from 'react';
 import BookList from '../containers/BookList';
 import BookForm from '../containers/BookForm';
+import './App.css';
 
 const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
 
